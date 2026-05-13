250b193d3ea103bba2479c986a30e164c884141f
Mubarak1980 <101741909+Mubarak1980@users.noreply.github.com>
2026-05-13 22:40:35 +0300

    Update service worker cache version to v101

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 // Service Worker (MISSION APP - FINAL PRO)
 // ===============================
 
-const CACHE_NAME = 'mission-cache-v100';
+const CACHE_NAME = 'mission-cache-v101';
 
 const ASSETS = [
   './',
