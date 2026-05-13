e62b358692f60d63e1a45f805d67034dd06f3580
Mubarak1980 <101741909+Mubarak1980@users.noreply.github.com>
2026-05-13 23:06:09 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 // Service Worker (MISSION APP - FINAL PRO)
 // ===============================
 
-const CACHE_NAME = 'mission-cache-v105';
+const CACHE_NAME = 'mission-cache-v106';
 
 const ASSETS = [
   './',
