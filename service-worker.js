cff830c1c2636ce92048dbb794d6ee9befb13341
Mubarak1980 <101741909+Mubarak1980@users.noreply.github.com>
2026-05-16 21:21:59 +0300

    Update service worker to version 110

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // Single Cache (Stable + Native-like)
 // ==========================================
 
-const CACHE_NAME = 'mission-cache-v109';
+const CACHE_NAME = 'mission-cache-v110';
 
 // App shell (core files)
 const APP_SHELL = [
