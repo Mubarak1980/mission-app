ff4b58108d9da6283c68d6540718de82dde5adfe
Mubarak1980 <101741909+Mubarak1980@users.noreply.github.com>
2026-05-16 21:10:48 +0300

    Update service worker to version 108

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // Single Cache (Stable + Native-like)
 // ==========================================
 
-const CACHE_NAME = 'mission-cache-v107';
+const CACHE_NAME = 'mission-cache-v108';
 
 // App shell (core files)
 const APP_SHELL = [
